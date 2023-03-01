eca8e558520f5726a72a2fdb90a665d0831f1fd9
chenxingzhi <chen.xingzhi@beicaizs.com>
2023-03-01 17:24:26 +0800

    feat: init

---
+++ src/views/AboutView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+// import { onUnmounted } from 'vue'
+
+let map: Map<any, any> | null = new Map()
+
+for (let i = 0; i < 1000000; i++) {
+  map.set(i, { label: 'label' + i, value: i })
+}
+
+// onUnmounted(()=>{
+//   map = null
+// })
+</script>
